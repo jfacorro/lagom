b15a56459db1f2638c22cdca9f544100379a2742
Juan Facorro <juan.facorro@klarna.com>
2018-02-01 23:01:56 +0100

    Improve detection of utterances
    Record all the time but drop the recording when there is nothing said

--- priv/js/main.js
+++ priv/js/main.js
@@ -1,17 +1,14 @@
 var Main = function() {
   var self = this;
   var state  = {
-    recorder: null,
-    chunks: []
+    recorder: null
   };
   self.state   = state;
-  self.status  = null;
   self.message = null;
   self.bubble  = null;
   self.mime    = MediaRecorder.isTypeSupported('audio/webm')? 'audio/webm' : 'audio/ogg';
 
   var init = function() {
-    self.status  = document.getElementById('status');
     self.message = document.getElementById('message');
     self.bubble  = document.getElementById('bubble');
 
@@ -54,27 +51,46 @@ var Main = function() {
     var recorderOpts = {audioBitsPerSecond: 16000, mimeType: self.mime};
     state.recorder   = new MediaRecorder(stream, recorderOpts);
 
+    var non_silence = 0;
+    var chunks = [];
+
+    state.recorder.registerSound = function() {
+      non_silence++;
+    };
+
+    state.recorder.hasSound = function() {
+      return non_silence > 0;
+    };
+
+    state.recorder.restart = function() {
+      if(state.recorder.state == 'recording') {
+        state.recorder.stop();
+      }
+      state.recorder.start();
+    };
+
     state.recorder.ondataavailable = function(e) {
-      state.chunks.push(e.data);
+      chunks.push(e.data);
     };
 
     state.recorder.onstart = function(e) {
-      state.chunks = [];
+      non_silence = 0;
+      chunks = [];
     };
 
     state.recorder.onstop = function(e) {
-      bubble.innerHTML = "&nbsp;";
-      bubble.className = "processing";
+      if(state.recorder.hasSound()) {
+        bubble.innerHTML = "&nbsp;";
+        bubble.className = "processing";
 
-      var blob = new Blob(state.chunks, { 'type' : self.mime });
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '/speech', true);
-      xhr.onload = onSpeech;
-      xhr.send(blob);
+        var blob = new Blob(chunks, { 'type' : self.mime });
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/speech', true);
+        xhr.onload = onSpeech;
+        xhr.send(blob);
 
-      console.log(blob);
-
-      self.status.innerHTML = "Listening...";
+        console.log("Sending utterance");
+      }
     };
   };
 
@@ -83,31 +99,38 @@ var Main = function() {
     var context  = new AudioContext();
     var source   = context.createMediaStreamSource(stream);
     var analyser = context.createAnalyser();
-    analyser.fftSize = 32;
+    analyser.fftSize = 2048;
     analyser.minDecibels = -40;
 
     source.connect(analyser);
 
     const data = new Uint8Array(analyser.frequencyBinCount);
-    const silenceDelay = 500;
+    const silenceDelay = 1000;
     var silenceStart = performance.now();
+
+    var isSilence = function(data) {
+      return !data.some(v => v);
+    };
+
+    var isLongSilence = function (now, start) {
+      return (now - start) > silenceDelay;
+    };
 
     var detect = function(time) {
       requestAnimationFrame(detect);
 
-      var isRecording = state.recorder.state == "recording";
       analyser.getByteFrequencyData(data);
 
-      if (data.some(v => v)) {
-        if (!isRecording) {
-          state.recorder.start();
-          self.status.innerHTML = "Recording...";
-          console.log("started recording");
+      if (isSilence(data)) {
+        if(isLongSilence(time, silenceStart)) {
+          console.log("restart recording");
+          state.recorder.restart();
+          silenceStart = time;
         }
+      } else {
+        console.log("sound registered");
+        state.recorder.registerSound();
         silenceStart = time;
-      } else if (isRecording && time - silenceStart > silenceDelay){
-        console.log("stopped recording");
-        state.recorder.stop();
       }
     };
 
